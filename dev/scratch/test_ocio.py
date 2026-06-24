4552e817b6c3df70e6c8cdba012e888c7763fd83
Scatter World <scatterworld@example.com>
2026-06-24 18:36:39 +0300

    Remove dev folder from repository for pristine release structure

--- dev/scratch/test_ocio.py
+++
@@ -1,9 +0,0 @@
-import os
-import PyOpenColorIO as OCIO
-try:
-    config = OCIO.GetCurrentConfig()
-    print("Default display:", config.getDefaultDisplay())
-    display = config.getDefaultDisplay()
-    print("Views:", config.getViews(display))
-except Exception as e:
-    print(e)
